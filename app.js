a4a9975049e7fcf728160d6f37ce60c45288b92f
GioLorenzo <gio.lorenzo.lui@gmail.com>
2021-01-25 13:04:25 -0500

    speech recognition function and button event listeners

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ const recognition = new SpeechRecognition();
 recognition.continuous = true;
 recognition.lang = 'english';
 
+recognition.onstart = () => {
+  console.log('yayy voice is activated!');
+}
+
+btn.addEventListener('blur', () => recognition.start());
+
+btn.addEventListener('focus', () => {
+  recognition.onresult = (event1) => {
+    const current = event1;
+    const newContent = current.results[0][0].transcript;
+    content.textContent = "\"" + newContent + "\"";
+    readOutLoud(newContent);
+  }
+  recognition.stop();
+});
